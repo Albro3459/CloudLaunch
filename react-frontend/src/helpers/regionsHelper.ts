ead6d6fa5e94e8fc27228e440a801eb03fb40458
Alex Brodsky <abrodsky@maruccisports.com>
2026-02-17 16:45:39 -0600

    Update to supported t2.micro aws regions

--- react-frontend/src/helpers/regionsHelper.ts
+++ react-frontend/src/helpers/regionsHelper.ts
@@ -1,27 +1,27 @@
 import { collection, getDocs, getFirestore } from "firebase/firestore";
 
 export const aws_regions = [
+{ name: "India (Mumbai)", value: "ap-south-1" },
+{ name: "France", value: "eu-west-3" },
+{ name: "United Kingdom", value: "eu-west-2" },
+{ name: "Ireland", value: "eu-west-1" },
+{ name: "Japan (Osaka)", value: "ap-northeast-3" },
+{ name: "South Korea", value: "ap-northeast-2" },
+{ name: "Japan (Tokyo)", value: "ap-northeast-1" },
+{ name: "Canada", value: "ca-central-1" },
+{ name: "Brazil", value: "sa-east-1" },
+{ name: "Singapore", value: "ap-southeast-1" },
+{ name: "Australia (Sydney)", value: "ap-southeast-2" },
+{ name: "Germany", value: "eu-central-1" },
 { name: "Virginia", value: "us-east-1" },
 { name: "Ohio", value: "us-east-2" },
 { name: "California", value: "us-west-1" },
 { name: "Oregon", value: "us-west-2" },
-{ name: "Australia (Sydney)", value: "ap-southeast-2" },
-{ name: "Brazil", value: "sa-east-1" },
-{ name: "Canada", value: "ca-central-1" },
-{ name: "France", value: "eu-west-3" },
-{ name: "Germany", value: "eu-central-1" },
-{ name: "Hong Kong", value: "ap-east-1" },
-{ name: "India (Mumbai)", value: "ap-south-1" },
-{ name: "Ireland", value: "eu-west-1" },
-{ name: "Japan (Osaka)", value: "ap-northeast-3" },
-{ name: "Japan (Tokyo)", value: "ap-northeast-1" },
-// { name: "Mexico", value: "mx-central-1" }, // Doesn't support t2.micro
-{ name: "Singapore", value: "ap-southeast-1" },
-{ name: "South Africa", value: "af-south-1" },
-{ name: "South Korea", value: "ap-northeast-2" },
-{ name: "Sweden", value: "eu-north-1" },
+// { name: "Hong Kong", value: "ap-east-1" },               // Doesn't support t2.micro
+// { name: "Mexico", value: "mx-central-1" },               // Doesn't support t2.micro
+// { name: "South Africa", value: "af-south-1" },           // Doesn't support t2.micro
+// { name: "Sweden", value: "eu-north-1" },                 // Doesn't support t2.micro
 // { name: "United Arab Emirates", value: "me-central-1" }, // Doesn't support t2.micro
-{ name: "United Kingdom", value: "eu-west-2" },
 ];
 
 
