e6bfa106479a3ada19371b8797ac77c140295c25
Albro3459 <brodsky.alex22@gmail.com>
2026-03-26 12:02:35 -0700

    No longer need to check live aws regions that support a certain shape

--- lambda/SecureGet/lambda_function.py
+++ lambda/SecureGet/lambda_function.py
@@ -3,13 +3,12 @@ from firebase_admin import auth, firestore
 
 from firebase import initialize_firebase, verify_firebase_token, get_user_role
 from get_secrets import get_secret
-from aws import get_enabled_regions, supports_instance_in_region, get_supported_regions
 
 SOURCE_REGION = "us-west-1"
 
 def lambda_handler(event, context):
     """
-    An API to securely get secrets and supported AWS regions from AWS
+    An API to securely get secrets from AWS
     """
     headers = event.get("headers", {})
     auth_header = headers.get("Authorization", headers.get("authorization", "")).strip() # AWS is case-sensitive
@@ -24,18 +23,23 @@ def lambda_handler(event, context):
         }
 
     # Extract required values
-    aws_regions = body.get("aws_regions", False)
     requested_keys = body.get("requested_keys", [])
 
     # Validate input
-    get_aws_regions = aws_regions is True
-    get_requested_keys = isinstance(requested_keys, list) and len(requested_keys) > 0
-    if not get_aws_regions and not get_requested_keys:
+    if not requested_keys or not isinstance(requested_keys, list):
+        print(f"Missing parameters: {requested_keys}")
         return {
             "statusCode": 400,
-            "body": json.dumps({"error": "Invalid request"})
+            "body": json.dumps({"error": f"Missing parameters"})
         }
-        
+
+    # Fetch secrets
+    secrets = get_secret(f"wireguard/config/{SOURCE_REGION}", SOURCE_REGION)
+    if not secrets:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": "Failed to retrieve secrets from AWS"})
+        }
     firebaseSecrets = get_secret("FirebaseServiceAccount", SOURCE_REGION)
     if not firebaseSecrets:
         return {
@@ -52,42 +56,18 @@ def lambda_handler(event, context):
     if not role: 
         return {"statusCode": 403, "body": json.dumps({"error": "No user role found"})}
     
-     # If regions and keys are requested, prioritize regions
-    if get_aws_regions:
-        # Fetch supported AWS regions
-        instance_type = "t2.micro"
-        
-        supported_regions = get_supported_regions(instance_type)
+    
+    # Get AWS Secrets
+    result = {}
+    for key in requested_keys:
+        if key == "client_private_key":
+            result[key] = secrets.get("CLIENT_PRIVATE_KEY")
+        elif key == "server_public_key":
+            result[key] = secrets.get("SERVER_PUBLIC_KEY")
+        else:
+            result[key] = None # Explicitly show unknown keys
 
-        print(f"Instance type {instance_type} is supported in these regions:")
-        for r in supported_regions:
-            print(r)
-
-        return {
-            "statusCode": 200,
-            "body": json.dumps(supported_regions)
-        }
-
-    elif get_requested_keys:       
-        # Get requested AWS Secrets
-        
-        secrets = get_secret(f"wireguard/config/{SOURCE_REGION}", SOURCE_REGION)
-        if not secrets:
-            return {
-                "statusCode": 500,
-                "body": json.dumps({"error": "Failed to retrieve secrets from AWS"})
-            }
-
-        result = {}
-        for key in requested_keys:
-            if key == "client_private_key":
-                result[key] = secrets.get("CLIENT_PRIVATE_KEY")
-            elif key == "server_public_key":
-                result[key] = secrets.get("SERVER_PUBLIC_KEY")
-            else:
-                result[key] = None # Explicitly show unknown keys
-
-        return {
-            "statusCode": 200,
-            "body": json.dumps(result)
-        }
+    return {
+        "statusCode": 200,
+        "body": json.dumps(result)
+    }
